93a02ac77405fe7f042fdb4bb358ffc5bc191314
chikuwa111 <take0shun1111@gmail.com>
2017-09-16 11:33:57 +0900

    Add sortable test component

---
+++ src/Sortable.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react'
+import {SortableContainer, SortableElement, arrayMove} from 'react-sortable-hoc'
+
+const SortableItem = SortableElement(({value}) => (
+  <li>{value}</li>
+))
+
+const SortableList = SortableContainer(({items}) => (
+  <ul>
+    {items.map((value, index) => (
+      <SortableItem key={value} index={index} value={value} />
+    ))}
+  </ul>
+))
+
+class SortableComponent extends Component {
+  state = {
+    items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'],
+  }
+
+  onSortEnd = ({oldIndex, newIndex}) => {
+    this.setState({
+      items: arrayMove(this.state.items, oldIndex, newIndex),
+    })
+  }
+
+  render() {
+    return (
+      <SortableList items={this.state.items} onSortEnd={this.onSortEnd.bind(this)} />
+    )
+  }
+}
+
+export default SortableComponent
