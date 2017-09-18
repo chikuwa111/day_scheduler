195b2f18266ea8cf731c7affba113c29aa255753
chikuwa111 <take0shun1111@gmail.com>
2017-09-18 13:17:40 +0900

    Change localStorage key

--- src/lib/localStorage.js
+++ src/lib/localStorage.js
@@ -1,6 +1,6 @@
 export const loadState = () => {
   try {
-    const serializedState = localStorage.getItem('state')
+    const serializedState = localStorage.getItem('day_schedule_state')
     if (serializedState === null) {
       return undefined
     }
@@ -13,7 +13,7 @@ export const loadState = () => {
 export const saveState = (state) => {
   try {
     const serializedState = JSON.stringify(state)
-    localStorage.setItem('state', serializedState)
+    localStorage.setItem('day_schedule_state', serializedState)
     return true
   } catch (err) {
     console.dir(err)
